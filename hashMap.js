40de6e6c027d2090ffdab69e80ef45e5dab10fc2
Aanas Aamer <anasamer056@gmail.com>
2024-03-07 23:28:59 +0200

    Implement remove(key)

--- hashMap.js
+++ hashMap.js
@@ -11,8 +11,7 @@ export default class HashMap {
 
     const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
-      hashCode =
-        (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
     }
 
     return hashCode;
@@ -34,51 +33,102 @@ export default class HashMap {
   }
 
   set(key, value) {
+    if (!key) return new Error("Key can't be null");
+
     if (this.load > this.loadFactor) {
       // Grow the capacity
     }
-    const hashCode = this.hash(key); 
-    
+    const hashCode = this.hash(key);
+
     this.insertAt(hashCode, new Node(key, value));
   }
 
-  insertAt(index, node){
+  insertAt(index, node) {
     if (index < 0 || index >= this.capacity) {
       throw new RangeError("Trying to access index out of bound");
     }
 
-    if (!this.data[index]){
+    if (!this.data[index]) {
       this.data[index] = node;
       return this;
     }
 
     let curr = this.data[index];
     let prev;
-    while (curr){
-      prev = curr; 
+    while (curr) {
+      prev = curr;
       curr = curr.next;
     }
 
     prev.next = node;
   }
 
-  entries(){
+  entries() {
     let res = [];
-    
-    for (let bucket of this.data){
-      if (!bucket) continue;
-      
-      let curr = bucket; 
 
-      while(curr){
+    for (let bucket of this.data) {
+      if (!bucket) continue;
+
+      let curr = bucket;
+
+      while (curr) {
         res.push([curr.key, curr.value]);
         curr = curr.next;
       }
     }
-    return res; 
+    return res;
   }
 
-  get load (){
+  get(key) {
+    if (!key || !this.data[this.hash(key)]) return null;
+
+    let curr = this.data[this.hash(key)];
+
+    while (curr) {
+      if (curr.key === key) return curr.value;
+      curr = curr.next;
+    }
+
+    return null;
+  }
+
+  has(key) {
+    if (this.get(key) !== null) return true;
+    return false;
+  }
+
+  remove(key) {
+    if (!key) return false;
+
+    const hashCode = this.hash(key);
+    if (!this.data[hashCode]) return false;
+
+    let index = 0;
+    let curr = this.data[hashCode];
+    let prev = curr;
+
+    while (curr) {
+      if (curr.key === key) {
+        if (!index) {
+          this.data[hashCode] = curr.next;
+        } else {
+          prev.next = curr.next;
+        }
+        return true;
+      }
+      index++;
+      prev = curr;
+      curr = curr.next;
+    }
+
+    return false
+  }
+
+  clear() {
+    this.data = [];
+  }
+
+  get load() {
     this.length / this.capacity;
   }
 }
--- index.js
+++ index.js
@@ -2,6 +2,11 @@ import HashMap from "./hashMap.js";
 
 const hashMap = new HashMap();
 hashMap.set("amber", "amer");
+hashMap.set("amberc", 7)
 hashMap.set("amberv", 4)
+hashMap.set("ambervv", 7)
 
-console.log(hashMap.entries());
+hashMap.set("ambervvv", 8)
+
+console.log(hashMap.remove("amberv"));
+console.log(hashMap.entries())
